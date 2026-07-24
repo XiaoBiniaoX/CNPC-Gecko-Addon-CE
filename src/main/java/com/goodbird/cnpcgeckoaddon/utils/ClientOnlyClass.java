b6502566435944c718dd75f6d3d10195ef789aa3
XiaoBiniaoX彬彬 <xiaobinzhao1145@gmail.com>
2026-07-25 03:20:35 +0800

    Delete src/main directory

--- src/main/java/com/goodbird/cnpcgeckoaddon/utils/ClientOnlyClass.java
+++
@@ -1,8 +0,0 @@
-package com.goodbird.cnpcgeckoaddon.utils;
-
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-@SideOnly(Side.CLIENT)
-public class ClientOnlyClass {
-}
